4aec3bcc52de0e52b596530de402b8fb6491bdae
razasoul08-png <razasoul08@gmail.com>
2025-08-22 14:41:28 +0500

    settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
   author: 'SUPREME',
-  botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
-  botOwner: 'Supreme', // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol.
+  botName: "Tech 𝐌ᴅ",
+  botOwner: 'tech', // Your name
+  ownerNumber: '923366803473', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
