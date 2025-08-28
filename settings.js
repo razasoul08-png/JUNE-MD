79b9009ca4fce0554eb43f573847357291fdcd93
razasoul08-png <razasoul08@gmail.com>
2025-08-28 19:09:02 +0500

    settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
   packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
   author: 'SUPREME',
-  botName: "Tech 𝐌ᴅ",
-  botOwner: 'tech', // Your name
+  botName: "fiza bot",
+  botOwner: 'fiza', // Your name
   ownerNumber: '923366803473', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
